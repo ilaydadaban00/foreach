647276088b40d02de1926ea2783c2ee69d25cf03
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2024-02-06 13:02:42 +0300

    Add files via upload

---
+++ foreach/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace @foreach
+{
+    public partial class Form1 : Form
+    {
+        List<int> sayilar = new List<int>() { 2,2,8,7,2,58,8,4,5,98};
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int toplam = 0;
+            foreach (int sayi in sayilar)
+                
+            {
+                toplam = toplam + sayi;
+                MessageBox.Show(toplam.ToString());
+                
+            }
+        }
+    }
+}
